6c063c39e2f045b31f54120a209dd7855d9dcfe9
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-04-23 23:05:25 +0300

    Experiments with colors cleanup

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-from raylib import *
+import raylib as rl
 
-InitWindow(800, 600, b"Raylib Test")
-while not WindowShouldClose():
-    BeginDrawing()
-    ClearBackground(RAYWHITE)
-    DrawText(b"Hello, Raylib!", 350, 300, 20, BLACK)
-    EndDrawing()
-CloseWindow()
+if __name__ == "__main__":
+    rl.InitWindow(800, 600, b"Raylib Test")
+    while not rl.WindowShouldClose():
+        rl.BeginDrawing()
+        rl.ClearBackground(rl.RAYWHITE)
+        rl.DrawText(b"Hello, Raylib!", 350, 300, 20, rl.BLACK)
+        rl.EndDrawing()
+    rl.CloseWindow()
