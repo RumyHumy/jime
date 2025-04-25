3b2edd5aae023dc9ab9054d1d46f22ed8a4fd0b6
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-04-25 22:48:33 +0300

    Added __pycache__ to .gitignore

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python3
 
-import raylib as rl
+# D E P E N D E N C I E S
+
+import os
+from raylib import rl, colors
+import importlib
+
+# A P P
+
+import draw
+
+# M A I N
+
+def main():
+    rl.SetConfigFlags(rl.FLAG_WINDOW_RESIZABLE)
+    rl.InitWindow(800, 600, b"Raylib Test")
+    draw_last_mod = os.stat("./draw.py").st_mtime
+    while not rl.WindowShouldClose():
+        draw.loopdraw()
+        # Hotreload
+        draw_curr_mod = os.stat("./draw.py").st_mtime
+        if draw_curr_mod > draw_last_mod:
+            importlib.reload(draw)
+            draw_last_mod = draw_curr_mod 
+    rl.CloseWindow()
 
 if __name__ == "__main__":
-    rl.InitWindow(800, 600, b"Raylib Test")
-    while not rl.WindowShouldClose():
-        rl.BeginDrawing()
-        rl.ClearBackground(rl.RAYWHITE)
-        rl.DrawText(b"Hello, Raylib!", 350, 300, 20, rl.BLACK)
-        rl.EndDrawing()
-    rl.CloseWindow()
+    main()
