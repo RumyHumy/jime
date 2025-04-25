a473fd86da335df3d5d869273f4ec33fdaa6bd2f
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-04-25 22:49:03 +0300

    Added file for hotreloading UI & sh1t

---
+++ draw.py
@@ -0,0 +1,8 @@
+from raylib import rl, colors
+
+def loopdraw():
+    global tim
+    rl.BeginDrawing()
+    rl.ClearBackground(colors.RAYWHITE)
+    rl.DrawText(b"KEKEK", 350, 300, 20, colors.BLACK)
+    rl.EndDrawing()
