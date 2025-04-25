32c12f13d2ac46dea76ffb404ec675ec01e1a579
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-04-25 23:56:40 +0300

    Added proper name for binary (imgjoy)

---
+++ build.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import PyInstaller.__main__
+
+if __name__ == "__main__":
+    PyInstaller.__main__.run([
+        'main.py',
+        'draw.py',
+        '--onefile',
+        '--name=Imgjoy',
+        '--clean'
+    ])
