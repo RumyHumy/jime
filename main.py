06352880832a842e086528abb564d111bf05ef43
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-04-25 23:53:59 +0300

    Added flag for disabling hotreload in binary

--- main.py
+++ main.py
@@ -6,23 +6,35 @@ import os
 from raylib import rl, colors
 import importlib
 
+BINARY_BUILD=False
+try:
+    import PyInstaller
+    BINARY_BUILD=True
+except ImportError:
+    pass
+
 # A P P
 
 import draw
 
 # M A I N
 
-def main():
-    rl.SetConfigFlags(rl.FLAG_WINDOW_RESIZABLE)
-    rl.InitWindow(800, 600, b"Raylib Test")
+if not BINARY_BUILD: # "draw.py" hotreloading
     draw_last_mod = os.stat("./draw.py").st_mtime
-    while not rl.WindowShouldClose():
-        draw.loopdraw()
-        # Hotreload
+    def draw_hotreload():
+        global draw_last_mod
         draw_curr_mod = os.stat("./draw.py").st_mtime
         if draw_curr_mod > draw_last_mod:
             importlib.reload(draw)
             draw_last_mod = draw_curr_mod 
+
+def main():
+    rl.SetConfigFlags(rl.FLAG_WINDOW_RESIZABLE)
+    rl.InitWindow(800, 600, b"Raylib Test")
+    while not rl.WindowShouldClose():
+        draw.loopdraw()
+        if not BINARY_BUILD:
+            draw_hotreload(draw_lat_mod)
     rl.CloseWindow()
 
 if __name__ == "__main__":
